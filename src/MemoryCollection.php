e38aab3124ccca6b3d0f76a0e833170eea570032
Hugo Blanco <hugo@liveecommerce.com.br>
2019-04-09 19:57:49 -0300

    Oopsie

--- src/MemoryCollection.php
+++ src/MemoryCollection.php
@@ -57,7 +57,7 @@ class MemoryCollection implements CollectionInterface
      */
     public function count(): int
     {
-        return count($this->data);
+        return count($this->data) + 1;
     }
 
     /**
